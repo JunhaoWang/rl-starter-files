6bac2b8b4dca845862f04d7de7222e63302afe8c
Hao <junhao@umich.edu>
2019-04-17 18:45:30 -0400

    incorporate flat model into codebase

---
+++ model_flat.py
@@ -0,0 +1,130 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions.categorical import Categorical
+import torch_ac
+import gym
+# note grid_seq_2_idx_seq should only be used when FullyObsWrapper is used
+from utils.misc import grid_seq_2_idx_seq
+
+# Function from https://github.com/ikostrikov/pytorch-a2c-ppo-acktr/blob/master/model.py
+def initialize_parameters(m):
+    classname = m.__class__.__name__
+    if classname.find('Linear') != -1:
+        m.weight.data.normal_(0, 1)
+        m.weight.data *= 1 / torch.sqrt(m.weight.data.pow(2).sum(1, keepdim=True))
+        if m.bias is not None:
+            m.bias.data.fill_(0)
+
+class ACModelFlat(nn.Module, torch_ac.RecurrentACModel):
+    def __init__(self, obs_space, action_space, use_memory=False, use_text=False):
+        super().__init__()
+
+        # Collect set of trajectory sequence information (state_sequence, state_space_cardinality, grid_shape,
+        # state_value_value) encoded by discrete state. Note it should only be used for discrete state when FullyObsWrapper
+        # is used.
+        self.traj_info_set = []
+
+        # Decide which components are enabled
+        self.use_text = use_text
+        self.use_memory = use_memory
+
+        
+        n = obs_space["image"][0]
+        m = obs_space["image"][1]
+        self.image_embedding_size = ((n-1)//2-2)*((m-1)//2-2)*64
+
+        
+
+        # Define memory
+        if self.use_memory:
+            self.memory_rnn = nn.LSTMCell(self.image_embedding_size, self.semi_memory_size)
+
+        # Define text embedding
+        if self.use_text:
+            self.word_embedding_size = 32
+            self.word_embedding = nn.Embedding(obs_space["text"], self.word_embedding_size)
+            self.text_embedding_size = 128
+            self.text_rnn = nn.GRU(self.word_embedding_size, self.text_embedding_size, batch_first=True)
+
+        # Resize image embedding
+        self.embedding_size = self.semi_memory_size
+        if self.use_text:
+            self.embedding_size += self.text_embedding_size
+
+        # Define image embedding
+        self.embedding_size = 64
+        size = n*m*3
+        self.fc  = nn.Sequential(
+            nn.Linear(size, 128),
+            nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Linear(64, self.embedding_size),
+            nn.ReLU()
+            )
+        #self.fc4 = nn.Linear(self.embedding_size, self.embedding_size)
+
+        # Define actor's model
+        if isinstance(action_space, gym.spaces.Discrete):
+            self.actor = nn.Sequential(
+                nn.Linear(self.embedding_size, 64),
+                nn.Tanh(),
+                nn.Linear(64, action_space.n)
+            )
+        else:
+            raise ValueError("Unknown action space: " + str(action_space))
+
+        # Define critic's model
+        self.critic = nn.Sequential(
+            nn.Linear(self.embedding_size, 64),
+            nn.Tanh(),
+            nn.Linear(64, 1)
+        )
+
+        # Initialize parameters correctly
+        self.apply(initialize_parameters)
+
+    @property
+    def memory_size(self):
+        return 2*self.semi_memory_size
+
+    @property
+    def semi_memory_size(self):
+        return self.image_embedding_size
+
+    def forward(self, obs, memory):
+        # note grid_seq_2_idx_seq should only be used when FullyObsWrapper is used
+        # idx_seq_info contains [state_idx_seq, state_idx_max, grid_shape, state_value_seq]
+        idx_seq_info = list(grid_seq_2_idx_seq(obs.image))
+
+        x = torch.transpose(torch.transpose(obs.image, 1, 3), 2, 3)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc(x)
+        #x = self.fc4(x)
+
+        if self.use_memory:
+            hidden = (memory[:, :self.semi_memory_size], memory[:, self.semi_memory_size:])
+            hidden = self.memory_rnn(x, hidden)
+            embedding = hidden[0]
+            memory = torch.cat(hidden, dim=1)
+        else:
+            embedding = x
+
+        if self.use_text:
+            embed_text = self._get_embed_text(obs.text)
+            embedding = torch.cat((embedding, embed_text), dim=1)
+
+        x = self.actor(embedding)
+        dist = Categorical(logits=F.log_softmax(x, dim=1))
+
+        x = self.critic(embedding)
+        value = x.squeeze(1)
+
+        self.traj_info_set.append(tuple(idx_seq_info))
+
+        return dist, value, memory
+
+    def _get_embed_text(self, text):
+        _, hidden = self.text_rnn(self.word_embedding(text))
+        return hidden[-1]
--- scripts/train_flat_full.py
+++ scripts/train_flat_full.py
@@ -9,8 +9,9 @@ import torch_ac
 import sys
 
 import utils
-from model import ACModel
+from model_flat import ACModelFlat
 from gym_minigrid.wrappers import FullyObsWrapper
+
 # Parse arguments
 
 parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
@@ -60,6 +61,7 @@ parser.add_argument("--recurrence", type=int, default=1,
                     help="number of timesteps gradient is backpropagated (default: 1)\nIf > 1, a LSTM is added to the model to have memory")
 parser.add_argument("--text", action="store_true", default=False,
                     help="add a GRU to the model to handle text input")
+parser.add_argument("--full-obs", type=int, default=0, help="full-obs")
 args = parser.parse_args()
 args.mem = args.recurrence > 1
 
@@ -92,12 +94,13 @@ if __name__ == '__main__':
     envs = []
     for i in range(args.procs):
         env = gym.make(args.env)
-        env = FullyObsWrapper(env)
+        if args.full_obs:
+            env = FullyObsWrapper(env)
         env.seed(args.seed + 10000*i)
         envs.append(env)
 
     # Define obss preprocessor
-
+    # print(envs[0].observation_space)
     obs_space, preprocess_obss = utils.get_obss_preprocessor(args.env, envs[0].observation_space, model_dir)
 
     # Load training status
@@ -113,7 +116,7 @@ if __name__ == '__main__':
         acmodel = utils.load_model(model_dir)
         logger.info("Model successfully loaded\n")
     except OSError:
-        acmodel = ACModel(obs_space, envs[0].action_space, args.mem, args.text)
+        acmodel = ACModelFlat(obs_space, envs[0].action_space, args.mem, args.text)
         logger.info("Model successfully created\n")
     logger.info("{}\n".format(acmodel))
 
@@ -188,10 +191,10 @@ if __name__ == '__main__':
 
             status = {"num_frames": num_frames, "update": update}
 
-        # Save vocabulary, model and status
+        # Save vocabulary and model
 
         if args.save_interval > 0 and update % args.save_interval == 0:
-            preprocess_obss.vocab.save()
+            #preprocess_obss.vocab.save()
 
             if torch.cuda.is_available():
                 acmodel.cpu()
