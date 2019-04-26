13bb7ab754bc1fd26e8e5bb9462a00f6ad9d9a26
dvVenuto <dv.venuto@gmail.com>
2019-04-26 11:55:34 -0400

    Updates needed to run

--- utils/getIndexedArrayFromTrajectory.py
+++ utils/getIndexedArrayFromTrajectory.py
@@ -5,30 +5,28 @@ def getIndexedArrayFromTrajectory(obs):
 	#states that are not real states
 	badStates=[2,3,4,5,6,7]
 	
-	trajTensor = torch.transpose(torch.transpose(obs, 1, 3), 2, 3)
-	trajTensor = np.array(trajTensor)
+	trajTensor = np.array(obs)
 	
 	#get dimensions of the tensor of trajectory
 	shapeParams = trajTensor.shape
-	print(shapeParams)
 	steps = shapeParams[0]
 	x_values = shapeParams[2]
 	y_values = shapeParams[3]
-	dir_values = shapeParams[1]
+	dir_values = shapeParams[4]
 
 	state_indexer = 0
-	
+
 	#The two dictionaries
 	stateToIndex = {}
 	indexToState = {}
 	
 	#to get state to tensor mappings
 	one_observation = trajTensor[0]
+	one_observation=one_observation[0]
 
 	for x in range(0,x_values-1):
 		for y in range(0,y_values-1):
-			state_code = one_observation[0][x][y]
-			print(state_code)
+			state_code = one_observation[x][y][0]
 			if state_code not in badStates:
 				stateToIndex[(x,y,0)] = state_indexer
 				indexToState[state_indexer] = (x,y,0)
@@ -48,24 +46,24 @@ def getIndexedArrayFromTrajectory(obs):
 	            
 def getStateIndexTraj(obs,stateToIndex,indexToState):
 
-	trajTensor = torch.transpose(torch.transpose(obs, 1, 3), 2, 3)
-	trajTensor = np.array(trajTensor)
+	trajTensor = np.array(obs)
 	
 	shapeParams = trajTensor.shape
 	steps = shapeParams[0]
 	x_values = shapeParams[2]
 	y_values = shapeParams[3]
-	dir_values = shapeParams[1]
+	dir_values = shapeParams[4]
 
 	state_sequence=[]
 	
 	for i in range(0,steps-1):
 		one_observation = trajTensor[i]
+		one_observation=one_observation[0]
 		for x in range(0,x_values-1):
 			for y in range(0,y_values-1):
-				state_code = one_observation[0][x][y]
+				state_code = one_observation[x][y][0]
 				if state_code == 10:
-					dir = one_observation[1][x][y]
+					dir = one_observation[x][y][1]
 					x_agent = x
 					y_agent = y
 					dir_agent = dir
