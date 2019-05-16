094db3afb6e8e0f54f8590b025025f543ffa7972
dvVenuto <dv.venuto@gmail.com>
2019-05-16 17:36:25 -0400

    remove memory leak

--- utils/vae.py
+++ utils/vae.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import gc
 import logging
 logging.getLogger('tensorflow').disabled = True
 import os
@@ -19,6 +20,7 @@ from scipy.stats import norm
 
 import random
 import numpy as np
+
 
 
 # reparameterization trick
@@ -229,5 +231,9 @@ def vaeGenerator(dataset, original_dim_inputs, intermediate_dim, latent_dim, bat
         z_sample=z_sample.reshape((1,latent_dim))
         x_decoded = generator.predict(z_sample)
         output_gen[i,] = x_decoded
-
+        
+    K.clear_session()
+    vae = None
+    del vae
+    gc.collect()
     return output_gen
